7d42d52396deecb97818c7d910becb22956e11a8
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-19 15:24:43 +0530

    commit

--- pass.py
+++ pass.py
@@ -1,33 +1,33 @@
 j=int(0)
 while j<3:
- con = int(0)
- name = input("enter the password ")
- l = int(len(name))
- if l <=int(4):
+  c1 = c2 = c3 = c4 = int(0)
+  name = input("enter the password ")
+  l = int(len(name))
+  if l <=int(4):
      print('password too short')
      j=j+1
      continue
- else:
+  else:
   
-  for i in range(l):
-    if ord(name[i])>=65 and ord(name[i])<=90:
-        con = con+1
-        break
-  for i in range(l):
-    if ord(name[i])>=97 and ord(name[i])<=122:
-        con = con+1
-        break
-  for i in range(l):
-    if ord(name[i])>=48 and ord(name[i])<=57:
-        con = con+1
-        break
-  for i in range(l):
-    if ord(name[i])>=33 and ord(name[i])<=47:
-        con = con+1
-        break
- if con >= 4:
+   for i in range(l):
+    if ord(name[i])>=65 and ord(name[i])<=90 :
+        c1=1
+  #       break
+  # for i in range(l):
+    if ord(name[i])>=97 and ord(name[i])<=122 :
+        c2=1
+  #       break
+  # for i in range(l):
+    if ord(name[i])>=48 and ord(name[i])<=57 :
+        c3=1
+  #       break
+  # for i in range(l):
+    if ord(name[i])>=33 and ord(name[i])<=47 :
+        c4=1
+        # break
+  if c1==c2==c3==c4==1:
      print('password created')
      break
- else:
+  else:
         j=j+1
         continue
--- password.py
+++
@@ -1,5 +0,0 @@
-passowrd = input("Enter the password ")
-for i in range(ord('a'),ord('z')):
-    for 
-        
-    

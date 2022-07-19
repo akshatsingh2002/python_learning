97de409f5897ffdf86c5413efd2f9d08d7ce4727
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-19 11:18:06 +0530

    commit

--- pass.py
+++ pass.py
@@ -1,6 +1,33 @@
-name = input("enter the password ")
-l = len(name)
-for i in range(l):
-    if ord(name[i])>=65 and ord(name[i])<=122:
-        print('yes')
+j=int(0)
+while j<3:
+ con = int(0)
+ name = input("enter the password ")
+ l = int(len(name))
+ if l <=int(4):
+     print('password too short')
+     j=j+1
+     continue
+ else:
+  
+  for i in range(l):
+    if ord(name[i])>=65 and ord(name[i])<=90:
+        con = con+1
         break
+  for i in range(l):
+    if ord(name[i])>=97 and ord(name[i])<=122:
+        con = con+1
+        break
+  for i in range(l):
+    if ord(name[i])>=48 and ord(name[i])<=57:
+        con = con+1
+        break
+  for i in range(l):
+    if ord(name[i])>=33 and ord(name[i])<=47:
+        con = con+1
+        break
+ if con >= 4:
+     print('password created')
+     break
+ else:
+        j=j+1
+        continue

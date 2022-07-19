d120b7e7da41bdd79d17a654b8cd676f29a24deb
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-19 18:11:26 +0530

    commit

--- stop.py
+++ stop.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from posixpath import split
 
 
@@ -6,15 +7,20 @@ lst2=[]
 i=0
 while i<1:
     imp = input("enter the method")
-    lst=split(imp)
+    lst=imp.split()
     met=lst[0]
-    value=lst[1]
+    if(met!="stop"):
+     value=lst[1]
+    print(lst)
     
-    if(imp=="push"):
+    if(met=="push"):
         lst2.append(value)
-    elif ( imp=="pop"):
-        lst2.remove(value)
-    elif (imp=="stop"):
+    elif ( met=="pop"):
+       u = lst2.remove(value)
+       if(u == NULL):
+           print('element does not exist')
+           break
+    elif (met=="stop"):
         break
     
 print(lst2)

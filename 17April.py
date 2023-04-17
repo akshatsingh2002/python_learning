af419b035033f67f2720977cfde360df7b7feb2c
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-17 17:16:57 +0530

    commit

---
+++ 17April.py
@@ -0,0 +1,13 @@
+# age = 200
+# print(age)
+# print(type(age))
+# name = "Akshat"
+# print(type(name))
+# print("akshat",end="")
+# name = "String"
+# print(type(name))
+# name = 'String'
+# print(type(name))
+# address = """elden ring 
+# won game of the year 2022"""
+# print(address)

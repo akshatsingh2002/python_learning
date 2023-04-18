161b46dfd43bb09f97680e53afdd861acaa0ca27
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-18 17:44:34 +0530

    commit

--- 18April.py
+++ 18April.py
@@ -4,3 +4,10 @@ print(id(age))
 print(id(salary))
 age = 20
 print(id(age))
+print("testing ", age)
+print(f"testing {age}")
+answer = f"this is string formatting{age}"
+print(answer)
+answer = "this is string formatting {}".format("val is here")
+print(answer
+      )

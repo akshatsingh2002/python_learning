061b598d60e25f69041e40f6149f4731c1ca238f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-19 15:09:28 +0530

    commit

--- 18April.py
+++ 18April.py
@@ -1,13 +1,18 @@
-age = 10
-salary = 10
-print(id(age))
-print(id(salary))
-age = 20
-print(id(age))
-print("testing ", age)
-print(f"testing {age}")
-answer = f"this is string formatting{age}"
-print(answer)
-answer = "this is string formatting {}".format("val is here")
-print(answer
-      )
+# age = 10
+# salary = 10
+# print(id(age))
+# print(id(salary))
+# age = 20
+# print(id(age))
+# print("testing ", age)
+# print(f"testing {age}")
+# answer = f"this is string formatting{age}"
+# print(answer)
+# answer = "this is string formatting {}".format("val is here")
+# print(answer)
+
+val = .5
+s = ("%0.10f"%(val)) #string fromatting . precision
+print(type(s))
+print(s)
+print(s[5:0:-1]) # starting position, ending position and step

2dc9c08ded9e6784cdbaf52d425e9be7753cebff
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-21 17:46:06 +0530

    commit

---
+++ April21.py
@@ -0,0 +1,9 @@
+num = 28
+if(num>25 and num<30                                                                                                                                                                                                                                                                                                                                                                                         ):
+    print("Number is greater than 20")
+elif(num>25):
+    print("Number is grater than 25")
+elif(num>25 and num<30):
+    print("NUmber is grater than 25 and less than 30")
+else:
+    print("NUMber is smaller than 30")

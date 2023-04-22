0b28010783bb0518eba273bd18116148e167cb1a
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-22 20:45:08 +0530

    commit

--- April21.py
+++ April21.py
@@ -1,10 +1,66 @@
-num = 28
-if(num>25 and num<30):
-    print("Number is grater than 25 and less than 30")
-elif(num>25):
-    print("Number is greater than 25")
-elif(num>20):
-    print("Number is greater than 20")
-else:
-    print("NUmber is less than 30")    
-                                                                                                                                                                                                                                                                                                                                                                                
+# num = 28
+# if(num>25 and num<30):
+#     print("Number is grater than 25 and less than 30")
+# elif(num>25):
+#     print("Number is greater than 25")
+# elif(num>20):
+#     print("Number is greater than 20")
+# else:
+#     print("NUmber is less than 30")   
+
+ 
+
+# val = input("Enter any string")
+# for i in range(0,len(val)):
+#     if(val[i] == 'a' or val[i] == 'A'):
+#         print(val[i],"Present at ",i)
+#     elif(val[i] == 'e' or val[i] == 'E'):
+#         print(val[i],"Present at ",i)
+#     elif(val[i] == 'i' or val[i] == 'I'):
+#         print(val[i],"Present at ",i)
+#     elif(val[i] == 'o' or val[i] == 'O'):
+#         print(val[i],"Present at ",i)
+#     elif(val[i] == 'u' or val[i] == 'U'):
+#         print(val[i],"Present at ",i)
+
+
+
+# val = input("Enter any string")
+# if(val[0] == 'a' or val[0] == 'A'):
+#     print(val[0],"Present at",0)
+# elif(val[0] == 'e' or val[0] == 'E'):
+#     print(val[0],"Present at",0)
+# elif(val[0] == 'i' or val[0] == 'I'):
+#     print(val[0],"Present at",0)
+# elif(val[0] == 'o' or val[0] == 'O'):
+#     print(val[0],"Present at",0)
+# elif(val[0] == 'u' or val[0] == 'U'):
+#     print(val[0],"Present at",0)
+# n = len(val)-1
+# if(val[n] == 'a' or val[n] == 'A'):
+#     print(val[n],"Present at",n)
+# elif(val[n] == 'e' or val[n] == 'E'):
+#     print(val[n],"Present at",n)
+# elif(val[n] == 'i' or val[n] == 'I'):
+#     print(val[n],"Present at",n)
+# elif(val[n] == 'o' or val[n] == 'O'):
+#     print(val[n],"Present at",n)
+# elif(val[n] == 'u' or val[n] == 'U'):
+#     print(val[n],"Present at",n)
+
+
+
+# val = int(input("Enter any integer: "))
+
+# for i in range(2,11):
+
+#     if(val%i==0):
+#         print(val," is divisible by ",i)
+
+
+
+# for i in range(5,0,-1):
+#     for j in range(0,i):
+#         print("*",end="")
+        
+#     print("")

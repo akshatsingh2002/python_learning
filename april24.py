38f2b9d18563a808797d30f3f0542b20384858de
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-25 09:00:04 +0530

    Commit

--- april24.py
+++ april24.py
@@ -13,7 +13,7 @@
 # else:
 #     print(num," is not prime")
 
-
+##NUmbers are called co-primes if their hcf is only one
 val1 = input("Enter the first value")
 val1 = int(val1)
 val2 = input("Enter the second value")
@@ -21,11 +21,11 @@ val2 = int(val2)
 num = max(val2,val1)
 con = 0
 for i in range(2,num//2+1):
-    if(val1%i==0 and val2%i==0 and con<=1):
-        con = con + 1
+    if(val1%i==0 and val2%i==0 ):
+        con = 1
+        break
         
-if(con == 1):
-    print(val1," ",val2"are coprimes")
+if(con == 0):
+    print(val1,val2,"Are co primes")
 else:
-    print(val1,val2"are o co primes")
-    
+    print(val1,val2,"Are not co primes")

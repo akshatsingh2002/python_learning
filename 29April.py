283034ca92411888e9c0b3f0d90fc3d7674f5cec
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-30 17:16:00 +0530

    commit

--- 29April.py
+++ 29April.py
@@ -1,4 +1,25 @@
-myset = {1,2,3,4}
-print(type(myset))
-myset.add((1,2,3,4))
-print(myset)
+# myset = {1,2,3,4}
+# print(type(myset))
+# myset.add((1,2,3,4))
+# print(myset)
+# myset.remove(2)
+# print(myset)
+# myset.discard(3)
+# print(myset)
+# myset.update([10,90,100])
+# print(myset)
+
+
+
+# val = input("Enter the string")
+# print(val)
+# mySet = set()
+# temp = ''
+# for i in range(0,len(val)):
+#     if(val[i]!=' '):
+#         temp = temp + val[i]
+#     else:
+#         mySet.add(temp)
+#         temp = ''
+        
+# print(mySet)

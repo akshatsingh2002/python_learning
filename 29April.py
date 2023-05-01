24d087d577c18614e9bcda3c11f7945c36037b97
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-01 08:17:36 +0530

    commit

--- 29April.py
+++ 29April.py
@@ -25,35 +25,50 @@
 # print(mySet)
 
 
-val = input("Enter the string")
-con1=0
-con2=0
-con3=0
-con4=0
-if(len(val)<6 or len(val)>15):
-    print("String length should be between 6 and 15")
+# val = input("Enter the string")
+# con1=0
+# con2=0
+# con3=0
+# con4=0
+# if(len(val)<6 or len(val)>15):
+#     print("String length should be between 6 and 15")
 
-else:
-        for i in val :
-            if(ord(i)>=48 and ord(i)<=57):
-                con1 = 1
-            if(ord(i)>=65 and ord(i)<=90):
-                con2 = 1
-            if(ord(i)>=97 and ord(i)<=122):
-                con3 = 1
-            if(i=='@' or i=='!' or i=='!' or i=='$'):
-                con4 = 1
+# else:
+#         for i in val :
+#             if(ord(i)>=48 and ord(i)<=57):
+#                 con1 = 1
+#             if(ord(i)>=65 and ord(i)<=90):
+#                 con2 = 1
+#             if(ord(i)>=97 and ord(i)<=122):
+#                 con3 = 1
+#             if(i=='@' or i=='!' or i=='!' or i=='$'):
+#                 con4 = 1
         
-        if(con1==0):
-            print("user should provide at least one numeric value")
-        elif(con2==0):
-            print("user should provide at least one upper case letter") 
-        elif(con3==0):
-            print("user should provide at least one lower case letter")
-        elif(con4==0):
-            print("user should provide at least one speical character")
-        else:
-            print("Password created successfully")   
+#         if(con1==0):
+#             print("user should provide at least one numeric value")
+#         elif(con2==0):
+#             print("user should provide at least one upper case letter") 
+#         elif(con3==0):
+#             print("user should provide at least one lower case letter")
+#         elif(con4==0):
+#             print("user should provide at least one speical character")
+#         else:
+#             print("Password created successfully")   
+
+
+
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if(i==1 or i==5):
+#             print("*",end='')
+#         elif(j==1 or j == 5):
+#             print("*",end='')
+#         else:
+#             print(" ",end='')
+        
+#     print('')    
+        
 
 
             

04b0039c41d5f083279a09a0b7f7d96a0eb467aa
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-02 09:29:59 +0530

    commit

--- may1.py
+++ may1.py
@@ -23,13 +23,33 @@
 
 
     
-for i in range(0,5):
-    for k in range(0,i):
-        print(" ",end='')
-    for j in range(i*2,4*2-1):
-        if(j%2==0):
-            print("*",end='')
-        else:
-            print(" ",end='')
+# for i in range(0,5):
+#     for k in range(0,i):
+#         print(" ",end='')
+#     for j in range(i*2,4*2-1):
+#         if(j%2==0):
+#             print("*",end='')
+#         else:
+#             print(" ",end='')
         
-    print()
+#     print()
+
+
+
+val1 = input("Enter first number")
+val1 = int(val1)
+val2 = input("Enter second number")
+val2 = int(val2)
+
+if val1>val2:
+    max = val1
+    min = val2
+else:
+    max = val2
+    min = val1
+    
+if(max%min==0):
+    print(max)
+else:
+    print(max*min)
+    

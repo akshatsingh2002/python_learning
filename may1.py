88d59ba9810b185ce64a89d253fec3956cc40350
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-02 09:14:18 +0530

    commit

---
+++ may1.py
@@ -0,0 +1,35 @@
+# for i in range(0,5):
+#     for k in range(0,5-i-1):
+#         print(" ",end='')
+#     for j in range(0,i+1):
+#         print("*",end='')
+        
+#     print()
+    
+    
+
+    
+    
+# for i in range(0,5):
+#     for k in range(0,5-i-1):
+#         print(" ",end='')
+#     for j in range(0,i*2-1):
+#         if(j%2==0):
+#             print("*",end='')
+#         else:
+#             print(" ",end='')
+        
+#     print()
+
+
+    
+for i in range(0,5):
+    for k in range(0,i):
+        print(" ",end='')
+    for j in range(i,4*2-1):
+        if(j%2==0):
+            print("*",end='')
+        else:
+            print(" ",end='')
+        
+    print()

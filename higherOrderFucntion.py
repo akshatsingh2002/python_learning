352db37b8bfcb33d57fdfe0e4ec104bbe6fbc0d9
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-08 21:20:30 +0530

    commit

---
+++ higherOrderFucntion.py
@@ -0,0 +1,28 @@
+#lambda function
+
+
+# val = lambda x: x**2
+# print(val(9))
+#lambda has no return value
+
+# val = lambda x:x%2==0
+
+# print(val(13))
+
+
+# def circle(r,area,cir):
+#     val1 = area(r)
+#     val2 = cir(r)
+#     print(val1)
+#     print(val2)
+    
+# def area(r):
+#     return 3.141*r*r
+
+# def cir(r):
+#     return 2*3.141*r
+
+# circle(9,area,cir)
+
+lst = list(map(lambda x:x%2!=0,[1,2,3,4,5,6]))
+print(lst)

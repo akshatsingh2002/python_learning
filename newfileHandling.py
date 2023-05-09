01b0d8f2d5475623a6da8658096cf189b16042fe
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-09 16:45:17 +0530

    commit

--- newfileHandling.py
+++ newfileHandling.py
@@ -1,4 +1,13 @@
-f = open('testfile.txt','r')
-text = f.read()
-print(text)
-f.close()
+# f = open('testfile.txt','r')
+# # print(f.tell())
+# print(f.seek(3))
+# text = f.read()
+# print(f.tell())
+# print(text)
+# f.close()
+
+with open('testfile.txt','r') as f:
+    text = f.readlines()
+    print(text)
+# text = f.readline()
+# print(text)

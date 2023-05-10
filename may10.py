20f104d8bc4a9d3b6a0c281490dfd936a6453776
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-10 17:57:04 +0530

    commit

--- may10.py
+++ may10.py
@@ -7,16 +7,37 @@
 # obj = student()
  
  
-class student:
-    def __init__(self,val,name):
-        self.val= val
-        self.name = name
-        print(self.val,self.name)
+# class student:
+#     def __init__(self,val,name):
+#         self.val= val
+#         self.name = name
+#         print(self.val,self.name)
+#     def info(self):
+#         print(self.val,self.name)
+        
+# obj = student(19,"akshat")
+# obj2  = student(10,"aman")
+# obj.info()
+# obj2.info()
+        
+        
+class hobbies:
+    def __init__(self,sname,email,hobby):
+        self.hobby = []
+        self.sname = sname
+        self.email = email
+        (self.hobby).append(hobby)
+
     def info(self):
-        print(self.val,self.name)
+        print(self.sname,self.email,self.hobby)
         
-obj = student(19,"akshat")
-obj2  = student(10,"aman")
-obj.info()
-obj2.info()
         
+    def more(self,val):
+        (self.hobby).append(val)
+        
+        
+s1 = hobbies("akshat","akshat@gmail.com","swimming")
+s1.more("circket")
+s1.more("volleyball")
+
+s1.info()        

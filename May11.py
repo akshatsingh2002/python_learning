9fd190d9365a14af2e5e9f0c914650d8b7b8b0f0
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-11 17:37:48 +0530

    commit

--- May11.py
+++ May11.py
@@ -1,21 +1,52 @@
-class driver:
-    def __init__(self,name,number):
-        self.fname = name 
-        self.number = number
+# class driver:
+#     def __init__(self,name,number):
+#         self.fname = name 
+#         self.number = number
         
-    def info(self):
-        print(self.fname,self.number)
+#     def info(self):
+#         print(self.fname,self.number)
         
 
-class customer(driver):
-    def __init__(self, name, number,email):
-        super().__init__(name, number)
-        self.email = email
-    def info(self):
-        super().info() 
-        print(self.email)
+# class customer(driver):
+#     def __init__(self, name, number,email):
+#         super().__init__(name, number)
+#         self.email = email
+#     def info(self):
+#         super().info() 
+#         print(self.email)
     
     
-c1 = customer("akshat",123,"akshat@gmail.com")
-c1.info()
-print(c1.fname)
+# c1 = customer("akshat",123,"akshat@gmail.com")
+# c1.info()
+# print(c1.fname)
+
+##class variables
+
+# class CounterValue:
+#     count = 0
+#     def __init__(self):
+#        CounterValue.count += 1
+       
+       
+# c1 = CounterValue()
+# c2 = CounterValue()
+# print(CounterValue.count)
+
+
+# class DataScience:
+#     def info():
+#         print("this is the datascience class")
+        
+# class child(DataScience):
+#     def info1():
+#         super.info()
+        
+        
+
+# class child2(child):
+#     def info2():
+#          super.info1()
+        
+        
+# obj = child2()
+# obj.info2()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-    
+print(CounterValue.count)

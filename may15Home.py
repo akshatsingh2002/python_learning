d501f0bf15e02f68f940fbf526f2fefec3bda041
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-16 10:20:55 +0530

    commit

---
+++ may15Home.py
@@ -0,0 +1,20 @@
+#fibonacci seraris
+
+# def fibo(val):
+#     a = 0
+#     b = 1
+#     temp = 0
+#     if(val == 0 or val == 1):
+#         print(1)
+#     else:
+#         print(1)
+#         for i in range(0,val-1):
+#             temp = a + b
+#             print(temp)
+#             a = b
+#             b = temp
+            
+
+# val = int(input("enter the number to print fibonacci series"))
+# fibo(val)
+

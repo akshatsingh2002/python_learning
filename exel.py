3a7101058ed04be37dbea9e8e9ee23da282e041c
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-16 17:38:11 +0530

    commit

--- exel.py
+++ exel.py
@@ -6,12 +6,19 @@
 # print(df)
 
 import openpyxl
+from openpyxl import Workbook
 path="Book1.xlsx"
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
 row = sheet_obj.max_row
 column = sheet_obj.max_column
 print(row,column)
-for i in range(1,row):
+for i in range(1,row+1):
     cell_obj = sheet_obj.cell(row = i , column = 1)
     print(cell_obj.value) 
+    
+for i in range(2,row+1):
+    cell_obj = sheet_obj.cell(row = i , column = 2)
+    cell_obj.value = "yes" 
+    
+wb_obj.save("Book1.xlsx")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-@classmethod
+import pandas as pd
+import openpyxl
+workbook = openpyxl.load_workbook("Book1.xlsx")
+df = pd.read_excel('Book1.xlsx')
+
+print(df)

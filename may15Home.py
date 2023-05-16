e4ac56bb50f7a571c73ed87f727066166dc450f9
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-16 10:33:37 +0530

    commit

--- may15Home.py
+++ may15Home.py
@@ -4,7 +4,7 @@
 #     a = 0
 #     b = 1
 #     temp = 0
-#     if(val == 0 or val == 1):
+#     if(val == 1):
 #         print(1)
 #     else:
 #         print(1)
@@ -18,3 +18,8 @@
 # val = int(input("enter the number to print fibonacci series"))
 # fibo(val)
 
+def fibo(val):
+    a = 0 , y  = 1
+    temp = 1
+    while i in range(0,val):
+        yield 

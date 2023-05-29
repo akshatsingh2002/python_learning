7cb17824dceaff5ee117c61c2929f8b3c6aed905
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-29 17:27:48 +0530

    commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,17 @@
-import numpy as np
-arr =np.array([1,2,3,4])
-print(arr.ndim)
+arr = numpy.array([[1,2,3],[4,5,6]])
+# arr = arr + 3
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
+# arr = arr  - 3 
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
+# arr = arr * 3
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
+# arr = arr / 3
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)

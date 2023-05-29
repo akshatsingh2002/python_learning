ce232c85d8f3fe3074be1c58ca62c1a6d549eece
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-29 17:48:27 +0530

    commit

--- mynumpy.py
+++ mynumpy.py
@@ -1,27 +1,27 @@
 import numpy 
 
-arr = numpy.array([[1,2,3],[4,5,6]])
-arr = arr + 3
-print(arr)
-print(arr.shape)
-print(arr.ndim)
-arr = arr  - 3 
-print(arr)
-print(arr.shape)
-print(arr.ndim)
-arr = arr * 3
-print(arr)
-print(arr.shape)
-print(arr.ndim)
-arr = arr / 3
-print(arr)
-print(arr.shape)
-print(arr.ndim)
+# arr = numpy.array([[1,2,3],[4,5,6]])
+# arr = arr + 3
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
+# arr = arr  - 3 
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
+# arr = arr * 3
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
+# arr = arr / 3
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
 
-arr = arr +[1,2,3]
-print(arr)
-print(arr.shape)
-print(arr.ndim)
+# arr = arr +[1,2,3]
+# print(arr)
+# print(arr.shape)
+# print(arr.ndim)
 
 # arr = arr +[1,2,3,4] blog here 
 # print(arr)
@@ -29,8 +29,12 @@ print(arr.ndim)
 # print(arr.ndim)
 
 
-# arr2 = numpy.array([1,2,3,4]) #first rank array
-# print(arr2.ndim) here
-# print(arr2.shape)
+arr2 = numpy.array([1,2,3,4])[numpy.newaxis] #first rank array
+# numpy.transpose(arr2)
+print(arr2.ndim) 
+print(arr2.shape)
+arr2 = numpy.transpose(arr2)
+print(arr2.ndim) 
+print(arr2.shape)
 
 # print(type(arr))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,1 @@
-arr = numpy.array([[1,2,3],[4,5,6]])
-# arr = arr + 3
-# print(arr)
-# print(arr.shape)
-# print(arr.ndim)
-# arr = arr  - 3 
-# print(arr)
-# print(arr.shape)
-# print(arr.ndim)
-# arr = arr * 3
-# print(arr)
-# print(arr.shape)
-# print(arr.ndim)
-# arr = arr / 3
-# print(arr)
-# print(arr.shape)
-# print(arr.ndim)
+numpy.transpose(arr2)

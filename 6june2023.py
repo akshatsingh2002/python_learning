2338a15fa0571052ea932dbd17662dd5b0403b32
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-06-06 16:28:16 +0530

    commit

--- 6june2023.py
+++ 6june2023.py
@@ -1,12 +1,21 @@
 import pandas as pd
 df = pd.read_csv("NbaInfo.csv")
-print(df)
-print(df.info())
-df["Name"] = df["Name"].astype("category")
-df["Salary"].fillna(0,inplace=True)
-df["Age"].fillna(0,inplace=True)
-df["Number"].fillna(0,inplace=True)
-df["Salary"]= df["Salary"].astype(int)
-df["Age"]= df["Age"].astype(int)
-df["Number"]= df["Number"].astype(int)
-print(df.info())
+df.dropna(how="any",inplace=True)
+# print(df)
+# print(df.info())
+# df["Name"] = df["Name"].astype("category")
+# df["Salary"].fillna(0,inplace=True)
+# df["Age"].fillna(0,inplace=True)
+# df["Number"].fillna(0,inplace=True)
+# df["Salary"]= df["Salary"].astype(int)
+# df["Age"]= df["Age"].astype(int)
+# df["Number"]= df["Number"].astype(int)
+# print(df.info())
+
+
+# print(df[df["Position"]=="PG"])
+# print(df[(df["Position"]=="PG")].count())
+
+
+print(df.groupby("Position").count())
+print(df.groupby("Position").first())
